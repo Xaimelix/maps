27085ebc4929c0e771650994ed2f07999b40e4b8
ivanov.aser <Xaimfolk355@gmail.com>
2023-02-04 17:39:30 +1000

    3

--- PyQt_applicaton.py
+++ PyQt_applicaton.py
@@ -2,23 +2,30 @@ import sys
 import os
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtWidgets import QLabel
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QImage
+from geocoder_request import geocode, get_map
 
 
 class Example(QWidget):
-    def __init__(self, image_path):
+    def __init__(self):
         super().__init__()
         self.initUI()
-        self.img = image_path
-
+        # self.img = image_path
+        self.Image_show()
     def initUI(self):
         self.setGeometry(500, 500, 500, 500)
         self.setWindowTitle('Map')
 
+        self.map = QLabel(self)
+        self.map.setGeometry(0, 0, 400, 400)
+
     def Image_show(self):
-        pic = QLabel(self)
-        pic.setPixmap(QPixmap(self.img))
-        pic.show()  # You were missing this.
+
+        data = get_map()
+        image = QImage()
+        image.loadFromData(data)
+        self.map.setPixmap(QPixmap(image))
+        # pic.show()  # You were missing this.
 
 
 if __name__ == '__main__':
--- geocoder_request.py
+++ geocoder_request.py
@@ -1,12 +1,30 @@
 import requests
 
-def geocode(human_address):
-    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+class Map():
+    def __init__(self):
+        self.map_type = 'map'
+    def geocode(self, human_address):
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
-    geocoder_params = {
-        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "geocode": human_address,
-        "format": "json"}
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": human_address,
+            "format": "json"}
 
-    response = requests.get(geocoder_api_server, params=geocoder_params)
-    return response
+        response = requests.get(geocoder_api_server, params=geocoder_params)
+        return response.content
+
+    def get_map(self):
+        api_server = "http://static-maps.yandex.ru/1.x/"
+
+        lon = "37.530887"
+        lat = "55.703118"
+        delta = "0.002"
+
+        params = {
+            "ll": ",".join([lon, lat]),
+            "spn": ",".join([delta, delta]),
+            "l": self.map_type
+        }
+        response = requests.get(api_server, params=params)
+        return response.content
