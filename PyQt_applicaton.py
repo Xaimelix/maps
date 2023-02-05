30c30be084726c398709d637d8c8c1a1fdba9fa8
Xami <Xaimfolk355@yandex.ru>
2023-02-05 21:48:51 +1000

    zoom

--- PyQt_applicaton.py
+++ PyQt_applicaton.py
@@ -1,11 +1,10 @@
 import sys
-import os
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtGui import QPixmap, QImage
+
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
-from PIL import *
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
 from geocoder_request import *
 
 
@@ -15,21 +14,40 @@ class App(QMainWindow):
         self.initUI()
         self.address = address
         self.map = Map()
+        self.btn_sputnik.setFocusPolicy(False)
+        self.btn_schema.setFocusPolicy(False)
+        self.btn_hybrid.setFocusPolicy(False)
+
         self.btn_sputnik.clicked.connect(self.buttons_change_mode)
         self.btn_schema.clicked.connect(self.buttons_change_mode)
         self.btn_hybrid.clicked.connect(self.buttons_change_mode)
         self.Image_show()
+        # self.coordinates = ''
 
     def initUI(self):
         uic.loadUi('map_window.ui', self)
 
-    def Image_show(self):
-        data = self.map.geocode(self.address)
-        image = QImage().fromData(data)
-        self.map_img.setPixmap(QPixmap(image))
+    def Image_show(self, update=False, ll=(0, 0)):
+        if not update:
+            data = self.map.geocode(self.address)
+            image = QImage().fromData(data)
+            self.map_img.setPixmap(QPixmap(image))
+            resp = self.map.get_map_info(self.address)
+            self.coordinates = resp['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point'][
+                'pos'].split(' ')
+        else:
+            coordinates = [float(i) for i in self.coordinates]
+            coordinates[0] += ll[0]
+            coordinates[1] += ll[1]
+            coordinates = [str(i) for i in coordinates]
+            self.coordinates = coordinates
+            data = self.map.get_map(coordinates[0], coordinates[1])
+            image = QImage().fromData(data)
+            self.map_img.setPixmap(QPixmap(image))
 
     def keyPressEvent(self, event):
         correct = True
+
         if event.key() == Qt.Key_PageUp or event.key() == Qt.Key_PageDown:
             if event.key() == Qt.Key_PageDown:
                 k_delta = 2
@@ -38,7 +56,16 @@ class App(QMainWindow):
                 k_delta = 0.5
                 correct = self.map.change_delta(k_delta)
             if correct:
-                self.Image_show()
+                self.Image_show(True)
+        if event.key() == Qt.Key_Right:
+            self.Image_show(True, (0.01, 0))
+        if event.key() == Qt.Key_Left:
+            self.Image_show(True, (-0.01, 0))
+        if event.key() == Qt.Key_Up:
+            self.Image_show(True, (0, 0.01))
+        if event.key() == Qt.Key_Down:
+            self.Image_show(True, (0, -0.01))
+        # self.Image_show()
 
     def buttons_change_mode(self):
         if self.sender() == self.btn_sputnik:
@@ -50,10 +77,6 @@ class App(QMainWindow):
         correct = self.map.change_mode(mode)
         if correct:
             self.Image_show()
-
-
-
-
 
 
 app = QApplication(sys.argv)
--- geocoder_request.py
+++ geocoder_request.py
@@ -20,22 +20,31 @@ class Map():
             "featureMember"][0]["GeoObject"]
         toponym_coodrinates = toponym["Point"]["pos"].split(' ')
 
-        data_bytes = self.get_map(toponym_coodrinates[0], toponym_coodrinates[1], 1)
+        data_bytes = self.get_map(toponym_coodrinates[0], toponym_coodrinates[1])
         return data_bytes
 
-    def get_map(self, lon, lat, zoom) -> bytes:
+    def get_map(self, lon, lat) -> bytes:
         api_server = "http://static-maps.yandex.ru/1.x/"
         self.lon = lon
         self.lat = lat
-        self.z = zoom
         params = {
             "ll": ",".join([self.lon, self.lat]),
             "spn": ",".join([self.delta, self.delta]),
-            "l": self.map_type,
-            "z": self.z
+            "l": self.map_type
         }
         response = requests.get(api_server, params=params)
         return response.content
+
+    def get_map_info(self, address):
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": address,
+            "format": "json"}
+
+        response = requests.get(geocoder_api_server, params=geocoder_params).json()
+        return response
 
     def change_delta(self, k_delta):
         if 0.00015 < float(self.delta) * k_delta < 10:
